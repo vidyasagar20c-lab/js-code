c8b12f20d2446416415918b18235711d45fbb01d
Vidya Sagar <vidyasagar20c@gmail.com>
2026-09-21 07:20:26 +0000

    functions

---
+++ 01_basic/06_date.js
@@ -0,0 +1,13 @@
+// Dates
+
+// let myDate = new Date(2045,3,31)
+// let myDate = new Date(2045,3,31,5,7)
+let myCreateDate = new Date("01-14-2026")
+// console.log(myDate.toLocaleString())
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+// console.log(Date.now()/1000)
+console.log(Math.floor((Date.now()/1000)))
---
+++ 03_basic/01_function.js
@@ -0,0 +1,63 @@
+// function sayMyName(){
+//     console.log("V")
+//     console.log("I")
+//     console.log("D")
+//     console.log("Y")
+//     console.log("A")
+//     console.log("S")
+//     console.log("A")
+//     console.log("G")
+//     console.log("A")
+//     console.log("R")
+// }
+// sayMyName()
+
+// function multiplyTwoNumber(number1, number2) {
+//    console.log( number1 + number2)
+// }
+// function addTwoNumber(number1, number2) {
+// //    let result = number1 + number2
+// return number1 + number2
+// }
+// let result = addTwoNumber(5,7)
+// console.log("Result : ", result)
+
+
+// function UserMassegelogin(username = "sam"){
+//     if(!username){
+//         console.log( `please Enter the name`)
+//         return 
+//     }
+//     return `${username} just logged `
+// }
+// console.log(UserMassegelogin())
+
+// function calculateCartPrice(...num1){
+//     return num1
+// }
+// console.log(calculateCartPrice(65,98,890,700))
+
+// let user = {
+//     name : "Vidya Sagar",
+//     price : 199
+// }
+
+// function handleuser(user){
+//     return user
+// }
+//     console.log(handleuser(`my User name is ${user.name} and price is ${user.price}`))
+
+// handleuser(user)
+// handleuser({
+//     // name : "ishwar chandar",
+//     // price : 299
+// })
+
+
+
+// let array = [200,300,400,598,500]
+
+// function arrayLenght(array){
+    // return array[2]
+// }
+// console.log(arrayLenght([200,300,400,598,500]))
